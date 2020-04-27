4dfbb30e2f8b94f2bb5a798160e08da2a26b9641
Andres Aguilar <andresaguilar@MacBook-Pro-de-Andres.local>
2020-04-27 14:59:01 -0300

    notification list order

--- server/notifications/controller.js
+++ server/notifications/controller.js
@@ -1,6 +1,6 @@
 const Notifications  = require('./model');
 
 exports.getList = async (req, res, next) => {
-  let list = await Notifications.findAll();
+  let list = await Notifications.findAll({order: [['createdAt', 'DESC']]});
   res.send(list);
 }
